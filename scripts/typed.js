827fecc05a9c507df13ad7d0ac744f6cfe3259f4
arkalsekar <abdulrehmankalsekar10@gmail.com>
2023-05-20 18:28:45 +0530

    Update the Site

---
+++ scripts/typed.js
@@ -0,0 +1,7 @@
+
+let typed = new Typed('#typedText', {
+    strings: ['I am a Student and I do Software Development, Data Science / Data Analytics, Machine Learning / DL, OpenCV / Computer Vision, and many more ... '],
+    typeSpeed: 10,
+    smartbackspace: true,
+    cursorChar: '_ ',
+});
